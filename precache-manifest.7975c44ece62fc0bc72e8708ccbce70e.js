6c21c73d5d3620c63db797fdc785e58fc1699715
ShubhamVerma1811 <imshubhamverma.sv@gmail.com>
2020-03-11 21:17:01 +0530

    Updates

---
+++ precache-manifest.7975c44ece62fc0bc72e8708ccbce70e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef35f4d2553e94f6625421d39f33f18a",
+    "url": "/QuizAppReact2/index.html"
+  },
+  {
+    "revision": "03191e834b439e8fef11",
+    "url": "/QuizAppReact2/static/css/main.80eee81d.chunk.css"
+  },
+  {
+    "revision": "d7cfd67b0d5de1643ef4",
+    "url": "/QuizAppReact2/static/js/2.ce9cc33b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/QuizAppReact2/static/js/2.ce9cc33b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03191e834b439e8fef11",
+    "url": "/QuizAppReact2/static/js/main.20d86eda.chunk.js"
+  },
+  {
+    "revision": "002d8bd3f78e64c473e2",
+    "url": "/QuizAppReact2/static/js/runtime-main.474ed33f.js"
+  }
+]);
